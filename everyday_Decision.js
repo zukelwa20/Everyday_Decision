a84e45517e8ed3cc36dd6fd42417abcc8c877fbe
bootcamp <makhosandilezukelwa@gmail.com>
2017-05-04 17:31:50 +0200

    second commit

--- everyday_Decision.js
+++ everyday_Decision.js
@@ -1,11 +1,21 @@
+var displayTable = document.querySelector(".displayTable");
+var searchButt = document.querySelector(".searchButt");
 var displayButt = document.querySelector(".displayButt");
 var myTemplate = document.querySelector(".myTemplate");
 var template = Handlebars.compile(myTemplate.innerHTML);
 
-var seasonObjs = [{season1: "Winter"},
-                 {season2: "Summer"},
-                 {season3: "Springs"},
-                 {season4: "Automn"}];
+var tableTemplate = document.querySelector(".tableTemplate");
+var tableTemp = Handlebars.compile(tableTemplate.innerHTML);
+
+var townObj =  [{town1: "CapeTown", Time:"13:00", Price:"R500"},
+                {town2: "East london", Time:"14:00", Price:"R600"},
+                {town3: "Alice", Time:"15:00", Price:"R400"},
+                {town4: "Fort Beaufort", Time:"16:00", Price:"R300"}];
 (function(){
-  displayButt.innerHTML = template({one: seasonObjs})
+  displayButt.innerHTML = template({radioBut: townObj})
 })()
+
+
+searchButt.addEventListener("click", function(){
+displayTable.innerHTML = tableTemp({townList: townObj})
+})
