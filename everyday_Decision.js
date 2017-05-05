8b0f3b7c272ffc7043394337ceace9d9d9551a6d
bootcamp <makhosandilezukelwa@gmail.com>
2017-05-05 17:14:32 +0200

    new commit

--- everyday_Decision.js
+++ everyday_Decision.js
@@ -6,16 +6,27 @@ var template = Handlebars.compile(myTemplate.innerHTML);
 
 var tableTemplate = document.querySelector(".tableTemplate");
 var tableTemp = Handlebars.compile(tableTemplate.innerHTML);
+var objectForOnlyTowns = [{town1: "Cape town"},
+                           {town2: "East london"},
+                           {town3: "Alice"},
+                           {town4: "Fort Beaufort"}];
 
-var townObj =  [{town1: "CapeTown", Time:"13:00", Price:"R500"},
-                {town2: "East london", Time:"14:00", Price:"R600"},
-                {town3: "Alice", Time:"15:00", Price:"R400"},
-                {town4: "Fort Beaufort", Time:"16:00", Price:"R300"}];
+var townObj =  [{town: "CapeTown",      Time:"13:00", Price:"R500"},
+                {town: "East london",   Time:"14:00", Price:"R600"},
+                {town: "Alice",         Time:"15:00", Price:"R400"},
+                {town: "Fort Beaufort", Time:"16:00", Price:"R300"}];
+
+var Arrival  =   [{town: "CapeTown",      Time:"13:00", Price:"R500"},
+                  {town: "East london",   Time:"14:00", Price:"R600"},
+                  {town: "Alice",         Time:"15:00", Price:"R400"},
+                  {town: "Fort Beaufort", Time:"16:00", Price:"R300"}];
+
+
 (function(){
-  displayButt.innerHTML = template({radioBut: townObj})
+  displayButt.innerHTML = template({radioBut: objectForOnlyTowns})
+
 })()
 
-
 searchButt.addEventListener("click", function(){
-displayTable.innerHTML = tableTemp({townList: townObj})
-})
+displayTable.innerHTML = tableTemp({townList: townObj,townList: Arrival})
+});
