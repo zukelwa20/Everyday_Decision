7ddb50a8bf9bdf1ad6244827a71333579a81c532
bootcamp <makhosandilezukelwa@gmail.com>
2017-05-15 16:58:20 +0200

    worked on js

--- everyday_Decision.js
+++ everyday_Decision.js
@@ -1,6 +1,7 @@
 var displayTable = document.querySelector(".displayTable");
 var searchButt = document.querySelector(".searchButt");
 var displayButt = document.querySelector(".displayButt");
+
 var myTemplate = document.querySelector(".myTemplate");
 var template = Handlebars.compile(myTemplate.innerHTML);
 
@@ -11,22 +12,52 @@ var objectForOnlyTowns = [{town1: "Cape town"},
                            {town3: "Alice"},
                            {town4: "Fort Beaufort"}];
 
-var townObj =  [{town: "CapeTown",      Time:"13:00", Price:"R500"},
-                {town: "East london",   Time:"14:00", Price:"R600"},
-                {town: "Alice",         Time:"15:00", Price:"R400"},
-                {town: "Fort Beaufort", Time:"16:00", Price:"R300"}];
+var townObj =  [{town: "CapeTown",      date: "03/05/20017", Time:"13:00", Price:"R500"},
+                {town: "East london",   date: "04/05/2017" , Time:"14:00", Price:"R600"},
+                {town: "Alice",         date: "05/05/2017" , Time:"15:00", Price:"R400"},
+                {town: "Fort Beaufort", date: "06/05/2017" , Time:"16:00", Price:"R300"}];
 
-var Arrival  =   [{town: "CapeTown",      Time:"13:00", Price:"R500"},
-                  {town: "East london",   Time:"14:00", Price:"R600"},
-                  {town: "Alice",         Time:"15:00", Price:"R400"},
-                  {town: "Fort Beaufort", Time:"16:00", Price:"R300"}];
+// var Arrival  =   [{town: "CapeTown",     Time:"13:00", Price:"R500"},
+//                   {town: "East london",   Time:"14:00", Price:"R600"},
+//                   {town: "Alice",         Time:"15:00", Price:"R400"},
+//                   {town: "Fort Beaufort", Time:"16:00", Price:"R300"}];
 
 
-(function(){
-  displayButt.innerHTML = template({radioBut: objectForOnlyTowns})
+// (function(){
+//   displayButt.innerHTML = template({radioBut: objectForOnlyTowns})
+//
+// })()
+//
+// searchButt.addEventListener("click", function(){
+//   displayTable.innerHTML = tableTemp({townList: townObj})
+// });
+var dateClass = document.querySelector(".dateClass");
+var timeClass = document.querySelector(".timeClass");
+var capeClass = document.querySelector(".capeClass");
+var eastClass = document.querySelector(".eastClass");
+var aliceClass = document.querySelector(".aliceClass");
+var fortClass = document.querySelector(".fortClass");
+var allButton = document.querySelector(".allButton");
 
-})()
-
-searchButt.addEventListener("click", function(){
-displayTable.innerHTML = tableTemp({townList: townObj,townList: Arrival})
+var Filter=[];
+allButton.addEventListener("click", function(){
+ for (var i = 0; i < objectForOnlyTowns.length; i++) {
+     var dataList = objectForOnlyTowns[i];
+     if(capeClass === dataList.town1 || capeClass === dataList.town1
+      && eastClass === dataList.town2 || eastClass === dataList.town2
+      && aliceClass === dataList.town3 || aliceClass === dataList.town3
+      && fortClass === dataList.town4 || fortClass === dataList.town4){
+       Filter.push(dataList);
+    }
+}
+displayButt.innerHTML = template({townList:objectForOnlyTowns})
 });
+
+// var allRadio = document.querySelector(".allRadio");
+// var selectedRadio = allRadio.value;
+// allRadio.addEventListener("click", function(){
+//
+//   function selectedRadio(input){
+//     return input.selectedRadio;
+//   }
+// })
