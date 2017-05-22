e22ac0c2789ea18956f70201ea760c1754adfb7b
bootcamp <makhosandilezukelwa@gmail.com>
2017-05-22 12:33:15 +0200

    made changes on my js

--- everyday_Decision.js
+++ everyday_Decision.js
@@ -1,63 +1,73 @@
 var displayTable = document.querySelector(".displayTable");
-var searchButt = document.querySelector(".searchButt");
+var search = document.querySelector(".searchButt");
 var displayButt = document.querySelector(".displayButt");
+var submitBut = document.querySelector(".submitBut");
+
+
+
+var tableTemplate = document.querySelector(".tableTemplate");
+var tableTemp = Handlebars.compile(tableTemplate.innerHTML);
 
 var myTemplate = document.querySelector(".myTemplate");
 var template = Handlebars.compile(myTemplate.innerHTML);
 
-var tableTemplate = document.querySelector(".tableTemplate");
-var tableTemp = Handlebars.compile(tableTemplate.innerHTML);
-var objectForOnlyTowns = [{town1: "Cape town"},
-                           {town2: "East london"},
-                           {town3: "Alice"},
-                           {town4: "Fort Beaufort"}];
-
-var townObj =  [{town: "CapeTown",      date: "03/05/20017", Time:"13:00", Price:"R500"},
-                {town: "East london",   date: "04/05/2017" , Time:"14:00", Price:"R600"},
-                {town: "Alice",         date: "05/05/2017" , Time:"15:00", Price:"R400"},
-                {town: "Fort Beaufort", date: "06/05/2017" , Time:"16:00", Price:"R300"}];
-
-// var Arrival  =   [{town: "CapeTown",     Time:"13:00", Price:"R500"},
-//                   {town: "East london",   Time:"14:00", Price:"R600"},
-//                   {town: "Alice",         Time:"15:00", Price:"R400"},
-//                   {town: "Fort Beaufort", Time:"16:00", Price:"R300"}];
+var WeekFlights = [{ day: "monday", time: "13H00", town: "East london"},
+                  { day: "Tuesday", time :  "14H00", town: "Queenstown"},
+                  { day: "Wenesday", time: "15H00", town: "Alice"},
+                  { day: "thursday", time: "16H00", town: "PE"},
+                  { day: "Friday", time:"17H00", town: "Durban"}]
 
 
-// (function(){
-//   displayButt.innerHTML = template({radioBut: objectForOnlyTowns})
-//
-// })()
-//
-// searchButt.addEventListener("click", function(){
-//   displayTable.innerHTML = tableTemp({townList: townObj})
-// });
-var dateClass = document.querySelector(".dateClass");
-var timeClass = document.querySelector(".timeClass");
-var capeClass = document.querySelector(".capeClass");
-var eastClass = document.querySelector(".eastClass");
-var aliceClass = document.querySelector(".aliceClass");
-var fortClass = document.querySelector(".fortClass");
-var allButton = document.querySelector(".allButton");
 
-var Filter=[];
-allButton.addEventListener("click", function(){
- for (var i = 0; i < objectForOnlyTowns.length; i++) {
-     var dataList = objectForOnlyTowns[i];
-     if(capeClass === dataList.town1 || capeClass === dataList.town1
-      && eastClass === dataList.town2 || eastClass === dataList.town2
-      && aliceClass === dataList.town3 || aliceClass === dataList.town3
-      && fortClass === dataList.town4 || fortClass === dataList.town4){
-       Filter.push(dataList);
-    }
-}
-displayButt.innerHTML = template({townList:objectForOnlyTowns})
+search.addEventListener("click", function(){
+displayTable.innerHTML = tableTemp({townList: WeekFlights})
 });
 
-// var allRadio = document.querySelector(".allRadio");
-// var selectedRadio = allRadio.value;
-// allRadio.addEventListener("click", function(){
-//
-//   function selectedRadio(input){
-//     return input.selectedRadio;
-//   }
-// })
+(function(){
+ displayButt.innerHTML = template({day: WeekFlights,
+                                   time: WeekFlights,
+                                  town: WeekFlights})
+})()
+
+var stored = document.querySelector(".stored")
+submitBut.addEventListener("click", function(){
+//  alert("hi");
+var dayClass = document.querySelector(".dayClass");
+var timeCLass = document.querySelector(".timeCLass");
+var townClass = document.querySelector(".townClass");
+
+var selectedDay = dayClass.value;
+var selectedTime = timeCLass.value;
+var selectedTown = townClass.vlue;
+
+function dayFilter(input){
+  return input.day == selectedDay;
+}
+function timeFilter(input){
+  return input.time == selectedTime;
+}
+function townFilter(input){
+  return input.town == selectedTown;
+}
+if(selectedDay !==""){
+  var newFlight = WeekFlights.filter(dayFilter);
+}
+if(selectedTime !==""){
+  if(selectedDay !==""){
+  var newFlight = newFlight.filter(timeFilter);
+}
+else{
+var newFlight = WeekFlights.filter(townFilter);
+}
+}
+if(selectedTime !==""){
+  if(selectedDay !=="" || selectedTown !==""){
+    var newFlight = newFlight.filter(timeFilter);
+  }
+  else{
+    var newFlight = WeekFlights.filter(timeFilter);
+  }
+}
+
+stored.innerHTML = tableTemp({townList: newFlight})
+})
